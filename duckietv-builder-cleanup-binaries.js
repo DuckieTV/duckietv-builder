89bf23cad89e1546d4d275cb228eaea1fdf36e06
Garfield69 <Garfield69@outlook.com>
2017-03-06 22:14:10 +1300

    cleanup-binaries #7

---
+++ duckietv-builder-cleanup-binaries.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+require('shelljs/global');
+var program = require('commander'),
+    sharedConfig = require('./shared');
+
+config.verbose = false;
+config.fatal = true;
+
+
+/**
+ * cleanup-binaries:
+ * - empties the binaries folder
+ */
+program
+    .description('empties the binaries folder')
+    .parse(process.argv);
+
+rm('-rf', sharedConfig.BINARY_OUTPUT_DIR);
+mkdir(sharedConfig.BINARY_OUTPUT_DIR);
+
+echo("Cleanup binaries performed.");

9880d5d903b743d56ae63e41297a738b45b64341
Js41637 <js41637@gmail.com>
2024-05-10 13:43:14 +1000

    add mac arm build support?

--- platforms/osx.js
+++ platforms/osx.js
@@ -6,10 +6,8 @@ var shared = require('../shared'),
  * The osx build processor makes a generic x64 
  */
 var BUILD_DIR = shared.BUILD_DIR + '/osx';
-var BINARY_OUTPUT_DIR = shared.BUILD_DIR + '/DuckieTV.app';
 var PACKAGE_FILENAME = 'DuckieTV-%VERSION%-OSX-%ARCHITECTURE%.pkg';
-var ARCHITECTURE = 'x64';
-
+var ARCHITECTURES = ['x64', 'arm64'];
 
 module.exports = {
 
@@ -24,40 +22,43 @@ module.exports = {
         },
 
         makeBinary: function(options) {
+            ARCHITECTURES.map(function(arch) {
+                var BINARY_OUTPUT_DIR = BUILD_DIR + "-" + arch + "/DuckieTV";
 
-            // default dir to copy the previously built duckietv sources to
-            var NWJS_APP_DIR = BINARY_OUTPUT_DIR + '/Contents/Resources/app.nw';
+                // default dir to copy the previously built duckietv sources to
+                var NWJS_APP_DIR = BINARY_OUTPUT_DIR + '/Contents/Resources/app.nw';
 
-            // download nwjs if not cached
+                // download nwjs if not cached
 
-            var EXTRACTED_NWJS = require('../nwjs-downloader')
-                .setDebug(options.nightly)
-                .setPlatform('osx')
-                .setVersion(shared.NWJS_VERSION)
-                .setArchitecture(ARCHITECTURE)
-                .get() + '/nwjs.app';
+                var EXTRACTED_NWJS = require('../nwjs-downloader')
+                    .setDebug(options.nightly)
+                    .setPlatform('osx')
+                    .setVersion(shared.NWJS_VERSION)
+                    .setArchitecture(arch)
+                    .get() + '/nwjs.app';
 
-            // cleanup
-            rm('-rf', BINARY_OUTPUT_DIR);
-            // re-init
-            mkdir('-p', BINARY_OUTPUT_DIR);
-            // copy default nwjs installation
-            cp('-r', EXTRACTED_NWJS + '/*', BINARY_OUTPUT_DIR);
-            // create folder app.nw in nwjs.app/Contents/Resources/
-            mv(BINARY_OUTPUT_DIR + '/Contents/MacOS/nwjs', BINARY_OUTPUT_DIR + '/Contents/MacOS/DuckieTV')
-            mkdir('-p', NWJS_APP_DIR);
-            // copy output/osx/* to nwjs.app/Contents/Resources/
-            cp('-r', BUILD_DIR + '/*', NWJS_APP_DIR)
+                // cleanup
+                rm('-rf', BINARY_OUTPUT_DIR);
+                // re-init
+                mkdir('-p', BINARY_OUTPUT_DIR);
+                // copy default nwjs installation
+                cp('-r', EXTRACTED_NWJS + '/*', BINARY_OUTPUT_DIR);
+                // create folder app.nw in nwjs.app/Contents/Resources/
+                mv(BINARY_OUTPUT_DIR + '/Contents/MacOS/nwjs', BINARY_OUTPUT_DIR + '/Contents/MacOS/DuckieTV')
+                mkdir('-p', NWJS_APP_DIR);
+                // copy output/osx/* to nwjs.app/Contents/Resources/
+                cp('-r', BUILD_DIR + '/*', NWJS_APP_DIR)
 
-            // copy osx/duckietv.icns to Contents/Resources/nw.icns
-            cp(__dirname + '/osx/duckietv.icns', BINARY_OUTPUT_DIR + '/Contents/Resources/app.icns');
+                // copy osx/duckietv.icns to Contents/Resources/nw.icns
+                cp(__dirname + '/osx/duckietv.icns', BINARY_OUTPUT_DIR + '/Contents/Resources/app.icns');
 
-            // patch Contents/info.plist with version number and stuff
-            cat(__dirname + '/osx/Info.plist')
-                .replace(/{{VERSION}}/g, shared.getVersion())
-                .replace(/{{NIGHTLY}}/g, options.nightly ? " Nightly" : "")
-                .to(BINARY_OUTPUT_DIR + "/Contents/Info.plist")
-            echo("Done making binary");
+                // patch Contents/info.plist with version number and stuff
+                cat(__dirname + '/osx/Info.plist')
+                    .replace(/{{VERSION}}/g, shared.getVersion())
+                    .replace(/{{NIGHTLY}}/g, options.nightly ? " Nightly" : "")
+                    .to(BINARY_OUTPUT_DIR + "/Contents/Info.plist")
+                echo("Done making binary");
+            })
         },
         packageBinary: function(options) {
 
@@ -78,61 +79,66 @@ module.exports = {
                 process.exit();
             }
 
-
-            var targetFileName = buildUtils.buildFileName(PACKAGE_FILENAME, ARCHITECTURE);
-            echo("Packaging: " + targetFileName);
-            var WORK_DIR = shared.BUILD_DIR + '/osx-installer';
-            var APP_BUILD_DIR = WORK_DIR + '/root/Applications/DuckieTV.app/';
-            var RESOURCE_DIR = __dirname + "/osx/build";
-
-            rm('-rf', WORK_DIR); // init work dir
-            mkdir('-p', WORK_DIR);
-            mkdir('-p', APP_BUILD_DIR);
-
-            cp('-r', RESOURCE_DIR + '/*', WORK_DIR); // initialize base directory structure
-            cp('-r', BINARY_OUTPUT_DIR + '/*', APP_BUILD_DIR); // copy source files to installer base
-            mkdir('-p', BINARY_OUTPUT_DIR);
-            cd(APP_BUILD_DIR);
-
-            // count files and install size and insert them in the install script
-            echo("Counting files: ");
-            var FILES_COUNT = exec("find " + WORK_DIR + "/root | wc -l").trim();
-            echo("Determining full install size in KB");
-            var LOCAL_INSTALL_KB_SIZE = exec("du -k -s " + WORK_DIR + "/root | awk '{print $1}'").trim();
-
-            cat(WORK_DIR + "/flat/Distribution")
-                .replace('{{VERSION}}', shared.getVersion())
-                .replace('{{NIGHTLY}}', options.nightly ? " Nightly" : "")
-                .replace('{{INSTALL_KB_SIZE}}', LOCAL_INSTALL_KB_SIZE)
-                .to(WORK_DIR + "/flat/Distribution");
-
-            // replace config variables in flat/base.pkg/PackageInfo and flat/Distribution
-            cat(WORK_DIR + "/flat/base.pkg/PackageInfo")
-                .replace('{{VERSION}}', shared.getVersion())
-                .replace('{{NIGHTLY}}', options.nightly ? " Nightly" : "")
-                .replace('{{INSTALL_KB_SIZE}}', LOCAL_INSTALL_KB_SIZE)
-                .replace('{{COUNT_FILES}}', FILES_COUNT)
-                .to(WORK_DIR + "/flat/base.pkg/PackageInfo");
-
-            // fix permissions
-            exec('chmod -R 775 ' + WORK_DIR + '/*');
-
-            // package payload
-
-            cd(WORK_DIR + "/root");
-            exec("(find . | cpio -o --format odc --owner 0:80 | gzip -c )> " + WORK_DIR + "/flat/base.pkg/Payload");
-
-            // create bill of materials
-            exec("mkbom -u 0 -g 80 . " + WORK_DIR + "/flat/base.pkg/Bom");
-            pushd(WORK_DIR + "/flat");
-            // store files using xar, save it as .pkg file
-            exec("xar --compression none -cf " + shared.BINARY_OUTPUT_DIR + "/" + targetFileName + " *");
-
-            echo("Done building for OSX, see " + shared.BINARY_OUTPUT_DIR + "/" + targetFileName);
-
+            ARCHITECTURES.map(function(arch) {
+                var BINARY_OUTPUT_DIR = BUILD_DIR + "-" + arch + "/DuckieTV";
+                var targetFileName = buildUtils.buildFileName(PACKAGE_FILENAME, arch);
+                echo("Packaging: " + targetFileName);
+                
+                var WORK_DIR = shared.BUILD_DIR + '/osx-installer';
+                var APP_BUILD_DIR = WORK_DIR + '/root/Applications/DuckieTV.app/';
+                var RESOURCE_DIR = __dirname + "/osx/build";
+    
+                rm('-rf', WORK_DIR); // init work dir
+                mkdir('-p', WORK_DIR);
+                mkdir('-p', APP_BUILD_DIR);
+    
+                cp('-r', RESOURCE_DIR + '/*', WORK_DIR); // initialize base directory structure
+                cp('-r', BINARY_OUTPUT_DIR + '/*', APP_BUILD_DIR); // copy source files to installer base
+                mkdir('-p', BINARY_OUTPUT_DIR);
+                cd(APP_BUILD_DIR);
+    
+                // count files and install size and insert them in the install script
+                echo("Counting files: ");
+                var FILES_COUNT = exec("find " + WORK_DIR + "/root | wc -l").trim();
+                echo("Determining full install size in KB");
+                var LOCAL_INSTALL_KB_SIZE = exec("du -k -s " + WORK_DIR + "/root | awk '{print $1}'").trim();
+    
+                cat(WORK_DIR + "/flat/Distribution")
+                    .replace('{{VERSION}}', shared.getVersion())
+                    .replace('{{NIGHTLY}}', options.nightly ? " Nightly" : "")
+                    .replace('{{INSTALL_KB_SIZE}}', LOCAL_INSTALL_KB_SIZE)
+                    .to(WORK_DIR + "/flat/Distribution");
+    
+                // replace config variables in flat/base.pkg/PackageInfo and flat/Distribution
+                cat(WORK_DIR + "/flat/base.pkg/PackageInfo")
+                    .replace('{{VERSION}}', shared.getVersion())
+                    .replace('{{NIGHTLY}}', options.nightly ? " Nightly" : "")
+                    .replace('{{INSTALL_KB_SIZE}}', LOCAL_INSTALL_KB_SIZE)
+                    .replace('{{COUNT_FILES}}', FILES_COUNT)
+                    .to(WORK_DIR + "/flat/base.pkg/PackageInfo");
+    
+                // fix permissions
+                exec('chmod -R 775 ' + WORK_DIR + '/*');
+    
+                // package payload
+    
+                cd(WORK_DIR + "/root");
+                exec("(find . | cpio -o --format odc --owner 0:80 | gzip -c )> " + WORK_DIR + "/flat/base.pkg/Payload");
+    
+                // create bill of materials
+                exec("mkbom -u 0 -g 80 . " + WORK_DIR + "/flat/base.pkg/Bom");
+                pushd(WORK_DIR + "/flat");
+                // store files using xar, save it as .pkg file
+                exec("xar --compression none -cf " + shared.BINARY_OUTPUT_DIR + "/" + targetFileName + " *");
+    
+                echo("Done building for OSX, see " + shared.BINARY_OUTPUT_DIR + "/" + targetFileName);
+    
+            })
         },
         publish: function(options) {
-            return [buildUtils.buildFileName(PACKAGE_FILENAME, 'x64')];
+            return ARCHITECTURES.map(function(arch) {
+                return buildUtils.buildFileName(PACKAGE_FILENAME, arch);
+            });
         }
 
     }
